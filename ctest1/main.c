4cc2db1de5871a74b4684e251c014afa96c89ad3
xinglianfly <2650454635@qq.com>
2016-06-20 09:46:02 +0800

    Initial Commit

---
+++ ctest1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ctest1
+//
+//  Created by xin wang on 16/6/20.
+//  Copyright © 2016年 xin wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
