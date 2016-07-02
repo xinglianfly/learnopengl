7514d17f4c2bbcd1bdd00272626fd20870502cb3
xinglianfly <2650454635@qq.com>
2016-07-02 08:58:24 +0800

    着色器

--- ctest1/main.c
+++ ctest1/main.c
@@ -6,6 +6,25 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+
+const GLchar* vertexShaderSource ="#version 330 core\n"
+    "layout(location=0) in vec3 position;\n"
+    "layout(location=1) in vec3 color;\n"
+    "out vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(position,1.0);\n"
+    "ourColor = color;\n"
+    "}\n\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 color;\n"
+    "uniform vec4 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "color = ourColor;\n"
+    "}\n\0";
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
     // 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
@@ -35,7 +54,70 @@ int main()
     glfwMakeContextCurrent(window);
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
+    
+    //顶点着色器
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    // Check for compile time errors
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+    }
+    
+    //片着色器
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+    }
    
+    //link shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    
+    // Check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n");
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    
+    GLfloat vertices[] = {
+        -0.5f,-0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.0f,0.5f,0.0f
+        
+    };
+    
+    GLuint VBO,VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+
     
     while (!glfwWindowShouldClose(window) ) {//每一次循环开始前检查一次GLFW是否准备好要退出，如果这样的话，返回true然后游戏循环就结束了
         glfwPollEvents();//有没有触发什么事件，然后调用对应的回调函数，
@@ -44,11 +126,28 @@ int main()
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        //画三角形
+        glUseProgram(shaderProgram);
+        
+        //改变uniform颜色
+        GLfloat timeValue = glfwGetTime();
+        GLfloat greenValue = (sin(timeValue)/2) + 0.5;
+        GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        
+        
+        //画三角形
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        
 
         glfwSwapBuffers(window);//交换缓冲区（储存着GLFW窗口每一个像素颜色的缓冲区）
     }
     
-   
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     
     glfwTerminate();
     
